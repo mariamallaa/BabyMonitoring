4f89e2c81a292a4e2e122e3dc55216d4bbf9678b
mariamallaa <37220622+mariamallaa@users.noreply.github.com>
2020-03-25 18:09:43 +0200

    2 frames per second

--- opticalflowtest.py
+++ opticalflowtest.py
@@ -6,7 +6,7 @@ from commonfunctions import *
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import interactive
 import math  
- 
+
      
 
 
@@ -27,7 +27,7 @@ def calcdisplacement(signals,currentframe,p1):
 
 
 
-cap = cv.VideoCapture("D:\\breathing2\\breathing2.mp4")
+cap = cv.VideoCapture("C:\\Users\\Mariam Alaa\\Downloads\\5518996\\sleep dataset\\sleep dataset\\1 cyc\\rgb.avi")
 #D:\breathing2
 #D:\\GP\\good.mp4
 
@@ -42,8 +42,14 @@ lk_params = dict(winSize=(15, 15), maxLevel=2, criteria=(
 color = (0, 255, 0)
 
 # Take first frame and find corners in it
-ret, old_frame = cap.read()
+frameId = cap.get(1) #current frame number
 
+frameRate = cap.get(5) #frame rate
+
+dimensions=[348, 219, 1084, 1949]
+ret, old_frame = cap.read()
+#old_frame=old_frame[dimensions[0]:dimensions[2],dimensions[1]+400:dimensions[3]]
+show_images(old_frame)
 old_gray = cv.cvtColor(old_frame, cv.COLOR_BGR2GRAY)
 p0 = []
 
@@ -56,39 +62,39 @@ signals.append(p0)
 signals = np.asarray(signals)
 currentframe=1
 mask = np.zeros_like(old_frame)
-while(1):
+while(cap.isOpened()):
     
-   
+    frameId = cap.get(1) #current frame number
     ret, frame = cap.read()
+    if(frameId % math.floor(frameRate) == 0 or frameId % math.floor(frameRate) == math.floor(frameRate/2)):
+        #frame=frame[dimensions[0]:dimensions[2],dimensions[1]+400:dimensions[3]]
+        frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     
-    frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-   
-    p1, st, err = cv.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
-    print("P1")
-    print(p1.shape)
-    disp.append(calcdisplacement(signals,currentframe,p1))
-    currentframe+=1
-    good_new = p1[st==1]
-    good_old = p0[st==1]
-    
-    
-    for k, (new, old) in enumerate(zip(good_new, good_old)):
+        p1, st, err = cv.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
         
-        a, b = new.ravel()
-        c, d = old.ravel()
-        mask = cv.line(mask, (a, b), (c, d), color, 2)
-        frame = cv.circle(frame, (b, a), 5, color, -1)
+        disp.append(calcdisplacement(signals,currentframe,p1))
+        currentframe+=1
+        good_new = p1[st==1]
+        good_old = p0[st==1]
         
-    
+        
+        for k, (new, old) in enumerate(zip(good_new, good_old)):
+            
+            a, b = new.ravel()
+            c, d = old.ravel()
+            mask = cv.line(mask, (a, b), (c, d), color, 2)
+            frame = cv.circle(frame, (b, a), 5, color, -1)
+            
+        
 
-    output = cv.add(frame, mask)
-    old_gray = frame_gray.copy()
-    p0=p1
-    cv.imshow("sparse optical flow", output)
-    
-    if cv.waitKey(10) & 0xFF == ord('q'):
-        break
-    
+        output = cv.add(frame, mask)
+        old_gray = frame_gray.copy()
+        p0=p1
+        cv.imshow("sparse optical flow", output)
+        
+        if cv.waitKey(10) & 0xFF == ord('q'):
+            break
+        
 
 
 disp = np.asarray(disp)
@@ -96,11 +102,11 @@ disp = np.asarray(disp)
 
 disp2=disp.transpose()
 
-
-
+show_images([old_frame])
 
 for i in range(disp2.shape[0]): 
-    plt.plot(disp2[i],range(disp2.shape[1]))
+    plt.plot(range(disp2.shape[1]),disp2[i])
+    print(signals[0,i])
     plt.show()
 
 cap.release()
