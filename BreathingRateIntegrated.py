ee82aa08752aff8e688d055ab597ee4e0f555561
Maram Khattab <maram.khattab@hotmail.com>
2020-06-22 22:23:28 +0200

    added closest to average of sliding window

--- BreathingRateIntegrated.py
+++ BreathingRateIntegrated.py
@@ -21,87 +21,14 @@ from scipy import signal
 import math
 
 
-def optical_flow_harris_old(nxt, prev, p0):
-    shapex = prev.shape[0]
-    shapey = prev.shape[1]
-    # print(prev.shape)
-    borderType = cv.BORDER_CONSTANT
-    prev = cv.copyMakeBorder(prev, 14, 14, 14, 14, borderType)
-    nxt = cv.copyMakeBorder(nxt, 14, 14, 14, 14, borderType)
-    # print(prev.shape)
-    prev2 = np.zeros(prev.shape)
-    j = 0
-    for i in range(len(p0)):
-        j = j+1
-        prev2[int(p0[i][0][0]), int(p0[i][0][1])] = 1
-        # prev2
-    # print("Sum:",np.sum(prev2))
-    kernel_x = np.array([[-1., 1.], [-1., 1.]])
-    kernel_y = np.array([[-1., -1.], [1., 1.]])
-    kernel_t = np.array([[1., 1.], [1., 1.]])
-    w = 7
-    mode = 'same'
-    fx = signal.convolve2d(prev, kernel_x, boundary='symm', mode=mode)
-    fy = signal.convolve2d(prev, kernel_y, boundary='symm', mode=mode)
-    ft = signal.convolve2d(nxt, kernel_t, boundary='symm', mode=mode) + \
-        signal.convolve2d(prev, -kernel_t, boundary='symm', mode=mode)
-    u = np.zeros(prev.shape)
-    v = np.zeros(prev.shape)
-    p1 = []
-    p2 = []
-    h = 0
-    y = 0
-    z = 0
-    for i in range(w, prev.shape[0]):
-        for j in range(w, prev.shape[1]):
-            if(prev2[i-w, j-w] == 1):
-                # print("p")
-                # print(i, j)
-                l = i-w
-                m = j-w
-                z += 1
-                y = y+1
-                Ix = fx[i-w:i+w+1, j-w:j+w+1]
-                Iy = fy[i-w:i+w+1, j-w:j+w+1]
-                It = ft[i-w:i+w+1, j-w:j+w+1]
-                Ix = np.reshape(Ix, 225).T
-                Iy = np.reshape(Iy, 225).T
-                It = np.reshape(It, 225).T
-                b = -It
-                A = np.array([Ix, Iy]).T
-                nu = np.matmul(np.linalg.pinv(A), b)
-                u[i-w, j-w] = nu[0]
-                v[i-w, j-w] = nu[1]
-                np_arr1 = np.array([u[i, j]*math.cos(v[i, j])])
-                np_arr2 = np.array(u[i, j]*math.sin(v[i, j]))
-                p1.append([[u[i-w, j-w]*math.cos(v[i-w, j-w])],
-                           [u[i-w, j-w]*math.sin(v[i-w, j-w])]])
-                if(u[i-w, j-w]*math.cos(v[i-w, j-w])+p0[h][0][0] < shapex and u[i-w, j-w]*math.cos(v[i-w, j-w])+p0[h][0][0] >= 0 and u[i-w, j-w]*math.sin(v[i-w, j-w]) + p0[h][0][1] < shapey and u[i-w, j-w]*math.sin(v[i-w, j-w]) + p0[h][0][1] >= 0):
-                    p2.append([[u[i-w, j-w]*math.cos(v[i-w, j-w])+p0[h][0][0],
-                                u[i-w, j-w]*math.sin(v[i-w, j-w]) + p0[h][0][1]]])
-                else:
-
-                    #print("out of range")
-                    p2.append([[p0[h][0][0], p0[h][0][1]]])
-                h = h+1
-
-    # print("z", z)
-    # print("negative:", p2[-3])
-    # if z < 28:
-    #     print("p0", p0)
-    #     print(l, m
-    #           )
-    return p2
-
-
 def optical_flow_harris(nxt, prev, p0):
     shapex = prev.shape[0]
     shapey = prev.shape[1]
-    # print(prev.shape)
+
     borderType = cv.BORDER_CONSTANT
     prev = cv.copyMakeBorder(prev, 14, 14, 14, 14, borderType)
     nxt = cv.copyMakeBorder(nxt, 14, 14, 14, 14, borderType)
-    # print(prev.shape)
+
     prev2 = np.zeros(prev.shape)
     j = 0
 
@@ -158,32 +85,25 @@ def optical_flow_harris(nxt, prev, p0):
 
 def calcdisplacement(signals, currentframe, p1):
     disp = []
-    # print(p1.shape)
     for i in range(p1.shape[0]):
         dispxy = []
         x = p1[i, 0, 0]-signals[0, i, 0, 0]
         y = p1[i, 0, 1]-signals[0, i, 0, 1]
         dist = math.sqrt((x)**2 + (y)**2)
-        # dispxy.append([dist])
         disp.append(dist)
-    #print("displacement done")
 
     return disp
 
 
-def remove_noise(signals, lower, upper):
+def remove_noise(signals, age):
     filtered_signals = []
     for i in range(signals.shape[0]):
         nsamples = len(signals[i])
-        #t = np.linspace(0, nsamples/2, nsamples, endpoint=False)
-        # plt.plot(t, signals[i], label="Noisy")
-
-        #b, a = butter(5, [lower, upper], btype='band')
-        b, a = butter(5, [0.2, 0.6], btype='band')
+        if age >= 1.5:
+            b, a = butter(5, [0.2, 0.6], btype='band')
+        else:
+            b, a = butter(5, [0.4, 0.8], btype='band')
         filtered = lfilter(b, a, signals[i])
-        # plt.plot(t, filtered, label="filtered")
-        # plt.legend(loc='upper left')
-        # plt.show()
         filtered_signals.append(filtered)
 
     filtered_signals = np.asarray(filtered_signals)
@@ -194,7 +114,7 @@ def remove_noise(signals, lower, upper):
 def get_components_pca(signals):
     pca = PCA(n_components=5)
     pca_components = pca.fit_transform(signals)
-    return pca_components
+    return pca.explained_variance_, pca_components
 
 
 def get_components_ica(signals):
@@ -203,9 +123,9 @@ def get_components_ica(signals):
     return ica_components
 
 
-def get_rates(disp, lower, upper):
+def get_rates(disp, age):
     disp2 = disp.transpose()
-    # print(disp2.shape)
+    print(disp2.shape)
     f_s = 2
     differences = []
     for i in range(disp2.shape[0]):
@@ -215,10 +135,13 @@ def get_rates(disp, lower, upper):
     length = len(differences)
     differences = differences[int(0.25*length):int(0.75*length)+1]
     disp2 = disp2[differences]
-    # print(disp2.shape)
+    print(disp2.shape)
 
-    filtered_signals = remove_noise(disp2, lower, upper)
-    components = get_components_pca(filtered_signals)
+    filtered_signals = remove_noise(disp2, age)
+    #("filtered", filtered_signals.shape)
+    explained_variance, components = get_components_pca(filtered_signals)
+
+    #components = pca_pattern(filtered_signals)
 
     rates = []
     for i in range(components.shape[1]):
@@ -233,20 +156,22 @@ def get_rates(disp, lower, upper):
 
         offset = next((i for i, x in enumerate(freqs) if x > 0.15), None)
         rate = 60*freqs[np.argmax(psd[freqs > 0.15])+offset]
-        # if uncertainty < 2 and rate > 9:
-        rates.append([rate, uncertainty, variance])
+        #uncertaintyModified = np.max(psd[freqs > 0.15])/np.sum(psd)
+
+        rates.append([rate, uncertainty, variance,
+                      explained_variance[i]]
+                     )
 
     rates = np.asarray(rates)
     # print(rates.shape)
     rates = rates[rates[:, 1].argsort()]
-   # print(rates)
+    print(rates)
     return rates
 
 
 def featureNormalize(X):
     normalized_X = X
     mu = 0
-    # TODO 1: Fill the function featureNormalize(X)
     mu = np.mean(X, axis=0)
     normalized_X = X-mu
 
@@ -254,16 +179,13 @@ def featureNormalize(X):
 
 
 def pca(X):
-    # TODO 2: Fill the function PCA(X)
     covariance = np.cov(X.transpose())
-    # print(covariance)
     u, s, vh = np.linalg.svd(covariance, full_matrices=True)
     return u, s
 
 
 def projectData(X, U, K):
     Z = 0
-    # TODO 3: Fill the function projectData(X,U,K)
     Z = (U.transpose())@(X.transpose())
     Z = Z.transpose()
     Z = Z[:, 0:K]
@@ -272,11 +194,11 @@ def projectData(X, U, K):
 
 def pca_pattern(X):
     normalized_X, mu = featureNormalize(X)
-    print("shape=", X.shape, "normalized=", normalized_X.shape)
+
     u, s = pca(normalized_X)
-    print("eigenvectors=", u.shape)
+
     pca_components = projectData(normalized_X, u, 5)
-    print("components", pca_components.shape)
+
     return pca_components
 
 
@@ -286,11 +208,12 @@ def breathing_rate(video, feature_params, lk_params, results_file, age):
     if age >= 0 and age < 1:
         minRate = 30
         maxRate = 60
-    elif age >= 1 and age < 3:
+    elif age >= 1 and age < 2:
         minRate = 24
         maxRate = 40
-    elif age >= 3 and age < 6:
-        minRate = 22
+
+    elif age >= 2 and age < 6:
+        minRate = 17
         maxRate = 34
     elif age >= 6 and age < 18:
         minRate = 18
@@ -301,19 +224,17 @@ def breathing_rate(video, feature_params, lk_params, results_file, age):
 
     output_file = open(results_file, "w+")
     cap = cv.VideoCapture(video)
-    # Take first frame and find corners in it
     frameId = cap.get(1)  # current frame number
-
     frameRate = cap.get(5)  # frame rate
-
     ret, old_frame = cap.read()
 
     old_gray = cv.cvtColor(old_frame, cv.COLOR_BGR2GRAY)
-    # print(old_gray.shape)
-    #p0 = []
+    p0 = []
 
     p0 = cv.goodFeaturesToTrack(old_gray, mask=None, **feature_params)
-    p0 = np.flip(p0, axis=2)
+
+    #p0 = np.flip(p0, axis=2)
+
     signals = []
     disp = []
     signals.append(p0)
@@ -330,17 +251,19 @@ def breathing_rate(video, feature_params, lk_params, results_file, age):
         if(ret == False):
             break
         if(frameId % math.floor(frameRate) == 0 or frameId % math.floor(frameRate) == math.floor(frameRate/2)):
-            # frame=frame[dimensions[0]:dimensions[2],dimensions[1]+400:dimensions[3]]
+
             frames_count += 1
             frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-            # p1, st, err = cv.calcOpticalFlowPyrLK(
-            #     old_gray, frame_gray, p0, None, **lk_params)
+            p1, st, err = cv.calcOpticalFlowPyrLK(
+                old_gray, frame_gray, p0, None, **lk_params)
 
-            p1 = optical_flow_harris(frame_gray, old_gray, p0)
-            p1 = np.asarray(p1)
+            # p1 = optical_flow_harris(frame_gray, old_gray, p0)
+            # p1 = np.asarray(p1)
+
             if frames_count == 1:
-                disp.append(calcdisplacement(signals, currentframe, p1))
+                disp.append(calcdisplacement(
+                    signals, currentframe, p1))
                 disp = np.asarray(disp)
             else:
                 disp = np.vstack((disp, calcdisplacement(
@@ -348,23 +271,35 @@ def breathing_rate(video, feature_params, lk_params, results_file, age):
             if frames_count >= 60 and frames_count % 2 == 0:
 
                 components_rates = get_rates(
-                    disp, (minRate)/60, (maxRate+7)/60)
+                    disp, age)
+
                 if frames_count == 60:
-                    prev_rates.append(components_rates[0, 0])
-                    output_file.write(str(components_rates[0, 0])+"\n")
-                    print("current breathing rate=", components_rates[0, 0])
-                    if components_rates[0, 0] < minRate or components_rates[0, 0] > maxRate:
-                        print("DANGER")
+                    components_rates = components_rates[components_rates[:, 0] >= minRate]
+                    components_rates = components_rates[components_rates[:, 3].argsort()[
+                        ::-1]]
+                    current_rate = components_rates[0, 0]
 
                 else:
-                    rates_diff = np.absolute(
-                        components_rates[:, 0]-prev_rates[-1])
-                    current_rate = components_rates[np.argmin(rates_diff), 0]
-                    prev_rates.append(current_rate)
-                    print("current breathing rate=", current_rate)
-                    if current_rate < minRate or current_rate > maxRate:
-                        print("DANGER")
-                    output_file.write(str(current_rate)+"\n")
+
+                    window_length = min(3, len(prev_rates))
+                    window_avg = sum(prev_rates[-window_length:])/window_length
+                    lowest_uncertainty = components_rates[0, 0]
+                    print("best uncertainty", lowest_uncertainty)
+                    highest_variance = components_rates[components_rates[:, 3].argsort()[
+                        ::-1]][0, 0]
+                    print("best variance", highest_variance)
+
+                    # if np.absolute(lowest_uncertainty-prev_rates[-1]) < np.absolute(highest_variance-prev_rates[-1]):
+                    if np.absolute(lowest_uncertainty-window_avg) < np.absolute(highest_variance-window_avg):
+                        current_rate = lowest_uncertainty
+                    else:
+                        current_rate = highest_variance
+
+                prev_rates.append(current_rate)
+                print(current_rate)
+                if current_rate < minRate or current_rate > maxRate:
+                    print("DANGER")
+                output_file.write(str(current_rate)+"\n")
 
                 disp = disp[2:, :]
                 calculated += 1
@@ -373,7 +308,17 @@ def breathing_rate(video, feature_params, lk_params, results_file, age):
 
             output = cv.add(frame, mask)
             old_gray = frame_gray.copy()
-            p0 = p1
+
+            color = (0, 255, 0)
+            for k, (new, old) in enumerate(zip(p1, p0)):
+
+                a, b = new.ravel()
+                c, d = old.ravel()
+                mask = cv.line(mask, (int(a), int(b)),
+                               (int(c), int(d)), color, 2)
+                frame = cv.circle(frame, (int(b), int(a)), 5, color, -1)
+                p0 = p1
+
             cv.imshow("sparse optical flow", output)
 
             if cv.waitKey(10) & 0xFF == ord('q'):
@@ -384,7 +329,7 @@ def breathing_rate(video, feature_params, lk_params, results_file, age):
     cv.destroyAllWindows()
 
 
-feature_params = dict(maxCorners=50, qualityLevel=0.05,
+feature_params = dict(maxCorners=100, qualityLevel=0.05,
                       minDistance=30, blockSize=3)
 
 lk_params = dict(winSize=(15, 15), maxLevel=2, criteria=(
@@ -392,5 +337,5 @@ lk_params = dict(winSize=(15, 15), maxLevel=2, criteria=(
 
 # cap = cv.VideoCapture(
 #     "C:\\Users\\Maram\\Desktop\\GP2\\labeled dataset\\test\\4.avi")
-breathing_rate("C:\\Users\\Maram\\Desktop\\GP2\\labeled dataset\\test\\6.avi",
-               feature_params, lk_params, "results.txt", 30)
+breathing_rate("C:\\Users\\Maram\\Desktop\\GP2\\labeled dataset\\test\\back dim.mp4",
+               feature_params, lk_params, "results.txt", 0.9)
