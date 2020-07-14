86b859e523021de9fd812273c8f3d62a9a03af5b
Maram Khattab <maram.khattab@hotmail.com>
2020-07-14 17:17:30 +0200

    breathing rate APIs

---
+++ flaskapi.py
@@ -0,0 +1,58 @@
+import flask
+from flask import request, jsonify
+from breathing_frame import *
+import cv2 as cv
+import base64
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+feature_params = dict(maxCorners=100, qualityLevel=0.05,
+                      minDistance=30, blockSize=3)
+
+lk_params = dict(winSize=(15, 15), maxLevel=2, criteria=(
+    cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
+
+
+breathing_module = breathing_rate(feature_params, lk_params)
+#breathing_rate(feature_params, lk_params, 2.5)
+
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
+@app.route("/setup", methods=["POST"])
+def initiate():
+    data = request.form
+    age = float(data["Age"])
+    breathing_module.set_age(age)
+    print(breathing_module.age)
+    return "ok"
+
+
+@app.route("/newframe", methods=["POST"])
+def new_frame():
+    data = request.form
+    string = data['Frame']
+    img = base64.b64decode(string)
+    img_as_np = np.frombuffer(img, dtype=np.uint8)
+    frame = cv.imdecode(img_as_np, flags=1)
+    # print(frame.shape)
+
+    #cv.imwrite("frame.jpg", frame)
+    breathing_module.estimate_breathing_rate(frame)
+
+    return "received"
+
+
+@app.route('/breathing', methods=['POST'])
+def breathing():
+    print(request.form)
+    return "ok"
+
+
+app.run()
---
+++ main.py
@@ -0,0 +1,38 @@
+import requests
+import flask
+import cv2 as cv
+import base64
+import math
+
+output_file = open("results.txt", "w+")
+cap = cv.VideoCapture(
+    "C:\\Users\\Maram\\Desktop\\GP2\\labeled dataset\\test\\face and neck.mp4")
+
+requests.post("http://127.0.0.1:5000/setup", data={"Age": 2.5})
+frameId = cap.get(1)  # current frame number
+frameRate = cap.get(5)  # frame rate
+ret, frame = cap.read()
+encoded_frame = cv.imencode(".jpg", frame)[1]
+requests.post("http://127.0.0.1:5000/newframe",
+              data={"Frame": base64.b64encode(encoded_frame)})
+
+
+#breathing.estimate_breathing_rate(frame, output_file)
+while(ret):
+
+    frameId = cap.get(1)  # current frame number
+    ret, frame = cap.read()
+    if(ret == False):
+        break
+    if(frameId % math.floor(frameRate) == 0 or frameId % math.floor(frameRate) == math.floor(frameRate/2)):
+        encoded_frame = cv.imencode(".jpg", frame)[1]
+        requests.post("http://127.0.0.1:5000/newframe",
+                      data={"Frame": base64.b64encode(encoded_frame)})
+        cv.imshow("sparse optical flow", frame)
+
+        if cv.waitKey(10) & 0xFF == ord('q'):
+            break
+
+# print(breathing.prev_rates)
+cap.release()
+cv.destroyAllWindows()
