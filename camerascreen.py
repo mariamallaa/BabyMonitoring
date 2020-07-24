315d6308be5c2a7f56fa5580b3af3806ccca772d
unknown <mariam.alaaeldin.mahmoud@gmail.com>
2020-07-24 21:33:33 +0200

    integrating playing mode

--- camerascreen.py
+++ camerascreen.py
@@ -1,42 +1,36 @@
+'''
 import kivy.app
 import requests
 import kivy.clock
 import threading
-from kivy.app import App
+import urllib3
 from kivy.uix.screenmanager import Screen
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
-from kivy.graphics.vertex_instructions import Line
-
+from kivy.app import App
 
 class CameraScreen(Screen):
     def __init__(self, **kwargs):
         super(CameraScreen, self).__init__(**kwargs)
         self.num_images = 0
-        self.danger_zone_coordinates = []
-
+        self.danger_zone_coordinates=[]
+   
+        
     def cam_size(self):
         print("cam size")
         camera = self.ids['camera']
-        cam_width_height = {
-            'width': camera.resolution[0], 'height': camera.resolution[1]}
-        setup_data = {"x0": self.danger_zone_coordinates[0][0], "y0": self.danger_zone_coordinates[0]
-                      [1], "x1": self.danger_zone_coordinates[1][0], "y1": self.danger_zone_coordinates[1][1]}
+        cam_width_height = {'width': camera.resolution[0], 'height':camera.resolution[1]}
+        setup_data={ "x0":self.danger_zone_coordinates[0][0], "y0":self.danger_zone_coordinates[0][1], "x1":self.danger_zone_coordinates[1][0], "y1":self.danger_zone_coordinates[1][1]}
         ip_addr = self.ids['ip_address'].text
         #port_number = self.ids['port_number'].text
-        url = 'http://' + ip_addr
+        url = 'http://' + ip_addr  
         try:
-            self.ids['cam_size'].text = "Trying to Establish a Connection..."
-            requests.post(url + '/camSize', params=cam_width_height)
-            self.ids['cam_size'].text = "Done."
+            self.ids['camsize'].text = "Trying to Establish a Connection..."
+            requests.post(url+  '/camSize', params=cam_width_height)
+            self.ids['camsize'].text = "Done."
             self.current = "capture"
             print("setting up BB")
             requests.post(url+'/setup-bb', data=setup_data)
         except requests.exceptions.ConnectionError:
-            self.ids['cam_size'].text = "Connection Error! Make Sure Server is Active."
+            self.ids['camsize'].text = "Connection Error! Make Sure Server is Active."
 
     def capture(self):
         kivy.clock.Clock.schedule_interval(self.upload_images, 0.5)
@@ -76,20 +70,7 @@ class CameraScreen(Screen):
         print(touch)
         self.danger_zone_coordinates.append([touch.spos[0], touch.spos[1]])
         return super(CameraScreen, self).on_touch_down(touch)
-    '''
-    def on_touch_down(self, touch):
-        if self.collide_point(*touch.pos):
-            self.canvas.clear()
-
-            # calculate touch minus position of ModalView
-            touch_in_modal = (touch.x - self.pos[0], touch.y - self.pos[1])
-            print('touch : ' + str(touch.pos) + ', touch in modal: ' + str(touch_in_modal))
-            self.danger_zone_coordinates=[]
-            print(touch)
-            self.danger_zone_coordinates.append([int(touch_in_modal[0]),int(touch_in_modal[1])])
-        return super(CameraScreen, self).on_touch_down(touch)
-    '''
-
+ 
     def on_touch_up(self, touch):
         print(touch)
         touch.apply_transform_2d(self.to_local)
@@ -112,3 +93,17 @@ class CameraScreen(Screen):
                 print(response.text)
             except requests.exceptions.ConnectionError:
                 self.ids['capture'].text = "Connection Error! Make Sure Server is Active."
+    def on_enter(self):
+        self.ids['camera'].play = False
+'''
+from kivy.app import App
+from kivy.uix.screenmanager import Screen
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+
+class CameraScreen(Screen):
+    pass
+
+    
+       
--- playingscreen.py
+++ playingscreen.py
@@ -1,16 +1,18 @@
-
-import kivy.app
-import requests
-import kivy.clock
-import threading
 from kivy.app import App
 from kivy.uix.screenmanager import Screen
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
-from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
-from kivy.graphics.vertex_instructions import Line
+    
+
+import kivy.app
+import requests
+import kivy.clock
+import threading
+import urllib3
+from kivy.uix.screenmanager import Screen
+from kivy.app import App
+
 
 
 class PlayingScreen(Screen):
@@ -19,54 +21,54 @@ class PlayingScreen(Screen):
         self.num_images = 0
         self.danger_zone_coordinates = []
 
-    def cam_size(self):
+    def cam_sizeplay(self):
         print("cam size")
-        camera = self.ids['camera']
-        cam_width_height = {
-            'width': camera.resolution[0], 'height': camera.resolution[1]}
+        camera = self.ids['cameraplay']
+        cam_width_height = {'width': camera.resolution[0], 'height': camera.resolution[1]}
         setup_data = {"x0": self.danger_zone_coordinates[0][0], "y0": self.danger_zone_coordinates[0]
                       [1], "x1": self.danger_zone_coordinates[1][0], "y1": self.danger_zone_coordinates[1][1]}
-        ip_addr = self.ids['ip_address'].text
+        ip_addr = self.ids['ip_addressplay'].text
         #port_number = self.ids['port_number'].text
         url = 'http://' + ip_addr
         try:
-            self.ids['cam_size'].text = "Trying to Establish a Connection..."
+            self.ids['cam_sizeplay'].text = "Trying to Establish a Connection..."
             requests.post(url + '/camSize', params=cam_width_height)
-            self.ids['cam_size'].text = "Done."
+            self.ids['cam_sizeplay'].text = "Done."
             self.current = "capture"
             print("setting up BB")
             requests.post(url+'/setup-bb', data=setup_data)
         except requests.exceptions.ConnectionError:
-            self.ids['cam_size'].text = "Connection Error! Make Sure Server is Active."
+            self.ids['cam_sizeplay'].text = "Connection Error! Make Sure Server is Active."
 
-    def capture(self):
-        kivy.clock.Clock.schedule_interval(self.upload_images, 0.5)
+    def captureplay(self):
+        kivy.clock.Clock.schedule_interval(self.upload_imagesplay, 0.5)
 
-    def upload_images(self, *args):
+    def upload_imagesplay(self, *args):
 
         self.num_images = self.num_images + 1
         print("Uploading image", self.num_images)
-        camera = self.ids['camera']
+        camera = self.ids['cameraplay']
         print("Image Size ", camera.resolution[0], camera.resolution[1])
         print("Image corner ", camera.x, camera.y)
         pixels_data = camera.texture.get_region(
             x=0, y=0, width=camera.resolution[0], height=camera.resolution[1]).pixels
-        ip_addr = self.ids['ip_address'].text
+        ip_addr = self.ids['ip_addressplay'].text
         #port_number = self.ids['port_number'].text
-        url = 'http://' + ip_addr + '/'
+        url = 'http://' + ip_addr + '/playing'
         response = requests.get(url+"get-stats")
         print(response.text)
         files = {'media': pixels_data}
-        t = threading.Thread(target=self.send_files_server, args=(files, url))
+        url = 'http://' + ip_addr + '/'
+        t = threading.Thread(target=self.send_files_serverplay, args=(files, url))
         t.start()
 
-    def send_files_server(self, files, url):
+    def send_files_serverplay(self, files, url):
         try:
             requests.post(url, files=files)
             response = requests.get(url+"get-stats")
             print(response.text)
         except requests.exceptions.ConnectionError:
-            self.ids['capture'].text = "Connection Error! Make Sure Server is Active."
+            self.ids['captureplay'].text = "Connection Error! Make Sure Server is Active."
 
     def on_touch_down(self, touch):
         print(touch)
@@ -84,19 +86,8 @@ class PlayingScreen(Screen):
         print(touch)
         self.danger_zone_coordinates.append([touch.spos[0], touch.spos[1]])
         print("RELEASED!", self.danger_zone_coordinates)
-        self.cam_size()
+        self.cam_sizeplay()
         # self.capture()
         return super(PlayingScreen, self).on_touch_up(touch)
-
-    def getstat(self):
-        while True:
-            ip_addr = self.ids['ip_address'].text
-            #port_number = self.ids['port_number'].text
-            url = 'http://' + ip_addr + '/'
-
-            try:
-                self.ids['capture'].text = "Trying to Establish a Connection..."
-                response = requests.get(url+"get-stats")
-                print(response.text)
-            except requests.exceptions.ConnectionError:
-                self.ids['capture'].text = "Connection Error! Make Sure Server is Active."
+    def on_enter(self):
+        self.ids['cameraplay'].play = True
